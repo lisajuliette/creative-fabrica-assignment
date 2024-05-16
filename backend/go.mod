80ce7c3e8279e532d9365f468a3cb3fb7e55d2df
Lisa van Zyl <lisathesen@gmail.com>
2024-05-16 20:20:26 +0200

    Enable CORS

--- backend/go.mod
+++ backend/go.mod
@@ -4,5 +4,6 @@ go 1.22.3
 
 require (
 	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/rs/cors v1.11.0 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -5,11 +5,20 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 func main() {
     r := mux.NewRouter()
     r.HandleFunc("/api/creators", getTopCreators).Methods("GET")
+
+    c := cors.New(cors.Options{
+        AllowedOrigins:   []string{"http://localhost:3000"},
+        AllowCredentials: true,
+    })
+
+    handler := c.Handler(r)
+
     log.Println("Starting server on :8080...")
-    log.Fatal(http.ListenAndServe(":8080", r))
+    log.Fatal(http.ListenAndServe(":8080", handler))
 }
